6f994a3110e9db75d226c8de2a7cc9525ef4cc88
tokhi <tshafi.tokhi@gmail.com>
2019-11-12 16:37:14 +0100

    jumbotron styling with bootstrap and style-component

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,6 +1,25 @@
--Jumbotron
--TableOfContents/List
- -Items
-  -Preview/Thumbnail
- -ActiveItem
-  -Video
+import React, {Component} from 'react'
+import Jumbotron from './Jumbotron'
+class Home extends Component{
+	constructor(){
+		super()
+
+		this.state = {
+			course_modules: [
+				{id: 1, title: '1. Getting Started with Rails 6 and react', description: 'lorem test 12e', active: false},
+				{id: 2, title: '2. Rails 6 React trouble shooting ', description: 'lorem test 12e', active: false},
+				{id: 3, title: '3. Building a simple React app', description: 'lorem test 12e', active: false},
+				{id: 4, title: '4. Adding React Router DOM to your App ', description: 'lorem test 12e', active: false}
+			]
+		}
+	}
+	render(){
+		return(
+			<div> 
+				<Jumbotron/>
+			</div>
+			)
+	}
+}
+
+export default Home
---
+++ app/javascript/components/Home/Jumbotron.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+
+const Section = styled.section`
+	background-color: #d74234;
+	min-height: 558px;
+	padding: 100px 0;
+	color: #fff;
+`
+
+const Header = styled.h1`
+	color: #fff;
+	font-weight: 700;
+	font-size: 40px;
+	line-height: 52px;
+`
+
+const SubHead = styled.p`
+	font-size: 18px;
+	font-weight: 500;
+`
+
+const Button = styled.a`
+	display: inlinie-block;
+	text-decoration: none;
+	font-weight: bold;
+	color: #333 !important;
+	cursoer: pointer;
+	border-radius: 0;
+	background: #fff;
+	padding: 10px 20px;
+	font-size: 18px;
+	// width: 100%;
+	box-shadow: 0px 0px 0px 3px #473228,
+		-6px 6px #ef5f17,
+		-6px 6px 0px 3px #473228;
+`
+
+
+const Jumbotron = () => {
+	return (
+		<Section className="home-section--1">
+			<div className="container">
+			  <div className="row">
+			    <div className="col-sm-12 col-md-5">
+			      <div className="pt-4 mt-4">
+			      	<Header> React! - Rails Developers </Header>
+			      	<SubHead>Empower your rails App with React</SubHead>
+			      	<div className="cta-wrapper">
+			      		<Button className="btn fancy-btn">Get Started</Button>
+			      	</div>
+			      </div>
+			    </div>
+			    <div className="col-sm-12 col-md-7">
+			      <div className="pt-4 mt-4 text-center">
+			      	 <iframe width="500" height="315" src="https://www.youtube.com/embed/B0SxxHAImhc"></iframe> 
+			      </div>
+			    </div>
+			  </div>
+			</div>
+		</Section>
+	)
+}
+
+export default Jumbotron
