18cfc9574818b998033be5109f18f64e0612b6dd
tokhi <tshafi.tokhi@gmail.com>
2019-11-12 17:31:02 +0100

    table items

---
+++ app/javascript/components/Home/Table/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// a dumb component
+const Thumbnail = () => {
+	return(
+		<div>
+			<iframe width="100%" height="100%" src="https://www.youtube.com/embed/B0SxxHAImhc"></iframe>
+		</div>
+	)
+}
+
+export default Thumbnail
