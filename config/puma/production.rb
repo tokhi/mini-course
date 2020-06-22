1b9408cf0779c505d55ec69b1433d9e4661ac429
tokhi <shafi.tokhi@gmail.com>
2020-06-22 16:53:56 +0200

    server config

---
+++ config/puma/production.rb
@@ -0,0 +1,19 @@
+rails_env = "production"
+environment rails_env
+app_dir = "/home/tokhi/app/mini-course"
+
+bind  "unix://#{app_dir}/puma.sock"
+pidfile "#{app_dir}/puma.pid"
+state_path "#{app_dir}/puma.state"
+directory "#{app_dir}/"
+
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+
+workers 2
+threads 1,2
+
+daemonize false
+
+activate_control_app "unix://#{app_dir}/pumactl.sock"
+
+prune_bundler
